791bdc09ffc7dbae99751b55deae96f41484ec47
yjj <1172338783@qq.com>
2020-02-17 01:32:26 +0800

    注册界面mvp协议的提交

--- app/src/main/java/com/itheima/huanxinim/contract/RegisterContract.kt
+++ app/src/main/java/com/itheima/huanxinim/contract/RegisterContract.kt
@@ -6,10 +6,15 @@ package com.itheima.huanxinim.contract
  */
 interface RegisterContract {
     interface Presenter : BasePresenter {
-
+        fun register(userName:String,password:String,confirmPassword:String)
     }
 
     interface View {
-
+        fun onUserNameError()
+        fun onPasswordError()
+        fun onConfirmPasswordError()
+        fun onStartRegister()
+        fun onRegisterSuccess()
+        fun onRegisterFailed()
     }
 }
