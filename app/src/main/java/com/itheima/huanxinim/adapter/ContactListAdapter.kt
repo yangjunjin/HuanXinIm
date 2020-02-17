921f2d1a6d8da10071996c7700b09b0d4bec0ab3
yjj <1172338783@qq.com>
2020-02-17 21:59:51 +0800

    联系人删除

--- app/src/main/java/com/itheima/huanxinim/adapter/ContactListAdapter.kt
+++ app/src/main/java/com/itheima/huanxinim/adapter/ContactListAdapter.kt
@@ -6,12 +6,15 @@ import android.content.DialogInterface
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.hyphenate.chat.EMClient
 import com.itheima.huanxinim.R
 import com.itheima.huanxinim.data.ContactListItem
 import com.itheima.huanxinim.ui.activity.ChatActivity
 import com.itheima.huanxinim.widget.ContactListItemView
 import org.jetbrains.anko.AlertBuilder
+import org.jetbrains.anko.runOnUiThread
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 
 /**
  * author : yangjunjin
@@ -45,7 +48,17 @@ class ContactListAdapter(val context: Context, private val contactListItems: Mut
 
     //删除好友
     private fun deleteFriend(userName: String) {
+        EMClient.getInstance().contactManager().aysncDeleteContact(userName,object :EMCallBackAdapter(){
+            override fun onSuccess() {
+                super.onSuccess()
+                context.runOnUiThread { toast(context.getString(R.string.success)) }
+            }
 
+            override fun onError(p0: Int, p1: String?) {
+                super.onError(p0, p1)
+                context.runOnUiThread { toast(context.getString(R.string.error)) }
+            }
+        })
     }
 
     class ContactListItemViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {}
