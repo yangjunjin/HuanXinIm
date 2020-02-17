324468524ab533002ba3f9d86f9387c5818c358b
yjj <1172338783@qq.com>
2020-02-17 21:53:44 +0800

    联系人删除

--- app/src/main/java/com/itheima/huanxinim/adapter/ContactListAdapter.kt
+++ app/src/main/java/com/itheima/huanxinim/adapter/ContactListAdapter.kt
@@ -1,12 +1,16 @@
 package com.itheima.huanxinim.adapter
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.itheima.huanxinim.R
 import com.itheima.huanxinim.data.ContactListItem
 import com.itheima.huanxinim.ui.activity.ChatActivity
 import com.itheima.huanxinim.widget.ContactListItemView
+import org.jetbrains.anko.AlertBuilder
 import org.jetbrains.anko.startActivity
 
 /**
@@ -26,6 +30,22 @@ class ContactListAdapter(val context: Context, private val contactListItems: Mut
         contactListItemView.bindView(contactListItems[position])
         val userName = contactListItems[position].userName
         contactListItemView.setOnClickListener { context.startActivity<ChatActivity>("userName" to userName) }
+        contactListItemView.setOnLongClickListener {
+            val message = String.format(context.getString(R.string.delete_friend), userName)
+            AlertDialog.Builder(context).setTitle(context.getString(R.string.delete_friend_tip)).setMessage(message)
+                .setNegativeButton(context.getString(R.string.cancel), null)
+                .setPositiveButton(
+                    context.getString(R.string.ok),
+                    DialogInterface.OnClickListener { dialogInterface, i ->
+                        deleteFriend(userName)
+                    }).show()
+            true
+        }
+    }
+
+    //删除好友
+    private fun deleteFriend(userName: String) {
+
     }
 
     class ContactListItemViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {}
