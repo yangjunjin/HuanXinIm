4f28ef3b588095bb28befa7e1d34c0c10639ad5c
yjj <1172338783@qq.com>
2020-02-19 18:56:17 +0800

    发送消息发送

--- app/src/main/java/com/itheima/huanxinim/widget/SendMessageItemView.kt
+++ app/src/main/java/com/itheima/huanxinim/widget/SendMessageItemView.kt
@@ -1,6 +1,7 @@
 package com.itheima.huanxinim.widget
 
 import android.content.Context
+import android.graphics.drawable.AnimationDrawable
 import android.util.AttributeSet
 import android.view.View
 import android.widget.RelativeLayout
@@ -9,6 +10,8 @@ import com.hyphenate.chat.EMTextMessageBody
 import com.hyphenate.util.DateUtils
 import com.itheima.huanxinim.R
 import kotlinx.android.synthetic.main.view_send_message_item.view.*
+import org.jetbrains.anko.runOnUiThread
+import org.jetbrains.anko.toast
 import java.util.*
 
 /**
@@ -25,6 +28,32 @@ class SendMessageItemView(context: Context?, attrs: AttributeSet? = null) : Rela
     fun bindView(emMessage: EMMessage) {
         updateTimestamp(emMessage)
         updateMessage(emMessage)
+        updateProgress(emMessage)
+    }
+
+    /**
+     * 发送消息进度
+     */
+    private fun updateProgress(emMessage: EMMessage) {
+        emMessage.status().let {
+            when(it){
+                //正在发送中
+                EMMessage.Status.INPROGRESS->{
+                    sendMessageProgress.visibility = View.VISIBLE
+//                    val animationDrawable = sendMessageProgress.drawable as AnimationDrawable
+//                    animationDrawable.start()
+                }
+                EMMessage.Status.SUCCESS->{
+                    sendMessageProgress.visibility = View.GONE
+                    context.runOnUiThread { toast("发送成功") }
+                }
+
+                EMMessage.Status.FAIL->{
+                    sendMessageProgress.visibility = View.VISIBLE
+                    context.runOnUiThread { toast("发送失败") }
+                }
+            }
+        }
     }
 
     /**
