2e2a5d66fed36d5c1cdb0fbd807c0742eac9d2e9
yjj <1172338783@qq.com>
2020-02-19 14:05:03 +0800

    聊天的界面的UI

---
+++ app/src/main/java/com/itheima/huanxinim/adapter/TextWatcherAdapter.kt
@@ -0,0 +1,18 @@
+package com.itheima.huanxinim.adapter
+
+import android.text.Editable
+import android.text.TextWatcher
+import com.hyphenate.EMCallBack
+
+/**
+ * author : yangjunjin
+ * date : 2020/2/15 18:04
+ */
+open class TextWatcherAdapter : TextWatcher {
+    override fun afterTextChanged(p0: Editable?) {}
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+}
--- app/src/main/java/com/itheima/huanxinim/ui/activity/ChatActivity.kt
+++ app/src/main/java/com/itheima/huanxinim/ui/activity/ChatActivity.kt
@@ -2,14 +2,19 @@ package com.itheima.huanxinim.ui.activity
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.core.app.ActivityCompat
 import com.itheima.huanxinim.R
+import com.itheima.huanxinim.adapter.TextWatcherAdapter
 import com.itheima.huanxinim.base.BaseActivity
 import com.itheima.huanxinim.contract.ChatContract
 import com.itheima.huanxinim.contract.LoginContract
 import com.itheima.huanxinim.presenter.ChatPresenter
 import com.itheima.huanxinim.presenter.LoginPresenter
+import kotlinx.android.synthetic.main.activity_chat.*
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.header.*
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 
@@ -21,6 +26,24 @@ class ChatActivity : BaseActivity(), ChatContract.View{
 
     override fun init() {
         super.init()
+        initHeader()
+        initEditText()
+    }
 
+    private fun initEditText() {
+        edit.addTextChangedListener(object :TextWatcherAdapter(){
+            override fun afterTextChanged(p0: Editable?) {
+             send.isEnabled = !p0.isNullOrEmpty()
+            }
+        })
+    }
+
+    //初始化头部
+    private fun initHeader() {
+        back.setOnClickListener { finish() }
+        //获取聊天的用户名
+        val username = intent.getStringExtra("userName")
+        val titleString = String.format(getString(R.string.chat_with),username)
+        headerTitle.text = titleString
     }
 }
