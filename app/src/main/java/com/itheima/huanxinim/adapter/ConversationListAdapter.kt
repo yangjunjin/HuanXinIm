d7eb3856775a8a8a7597754f6869e676afbdb1d4
yjj <1172338783@qq.com>
2020-02-20 17:18:30 +0800

    会话列表UI的完成

--- app/src/main/java/com/itheima/huanxinim/adapter/ConversationListAdapter.kt
+++ app/src/main/java/com/itheima/huanxinim/adapter/ConversationListAdapter.kt
@@ -4,20 +4,21 @@ import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.itheima.huanxinim.data.db.Contact
+import com.hyphenate.chat.EMConversation
 import com.itheima.huanxinim.widget.ConversationListItemView
 
 /**
  * author : yangjunjin
  * date : 2020/2/20 16:47
  */
-class ConversationListAdapter(val context:Context): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class ConversationListAdapter(
+    val context: Context, val conversations: MutableList<EMConversation>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return   ConversationListItemViewHolder(ConversationListItemView(context))
     }
 
-    override fun getItemCount(): Int=30
+    override fun getItemCount(): Int=conversations.size
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
 
--- app/src/main/java/com/itheima/huanxinim/ui/fragment/ConversationFragment.kt
+++ app/src/main/java/com/itheima/huanxinim/ui/fragment/ConversationFragment.kt
@@ -2,17 +2,23 @@ package com.itheima.huanxinim.ui.fragment
 
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.hyphenate.chat.EMClient
+import com.hyphenate.chat.EMConversation
 import com.itheima.huanxinim.R
 import com.itheima.huanxinim.adapter.ConversationListAdapter
 import com.itheima.huanxinim.base.BaseFragment
 import kotlinx.android.synthetic.main.fragment_conversation.*
 import kotlinx.android.synthetic.main.header.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 /**
  * author : yangjunjin
  * date : 2020/2/17 14:54
  */
 class ConversationFragment:BaseFragment() {
+
+    val conversations = mutableListOf<EMConversation>()
     override fun getLayoutResId(): Int {
         return R.layout.fragment_conversation
     }
@@ -25,7 +31,19 @@ class ConversationFragment:BaseFragment() {
         recyclerView.apply {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(context)
-            adapter = ConversationListAdapter(context)
+            adapter = ConversationListAdapter(context,conversations)
+        }
+        loadConversations()
+    }
+
+    /**
+     * 下载会话列表
+     */
+    private fun loadConversations() {
+        doAsync {
+            val allConversations = EMClient.getInstance().chatManager().allConversations
+            conversations.addAll(allConversations.values)
+            uiThread { recyclerView.adapter?.notifyDataSetChanged() }
         }
     }
 }
