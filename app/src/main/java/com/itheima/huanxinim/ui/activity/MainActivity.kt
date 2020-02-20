231450bb761d09c725dd656bceb80ca94ad01010
yjj <1172338783@qq.com>
2020-02-20 17:55:05 +0800

    会话列表-消息-已读

--- app/src/main/java/com/itheima/huanxinim/ui/activity/MainActivity.kt
+++ app/src/main/java/com/itheima/huanxinim/ui/activity/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.itheima.huanxinim.ui.activity
 
+import android.util.Log
+import com.hyphenate.chat.EMClient
+import com.hyphenate.chat.EMMessage
 import com.itheima.huanxinim.R
+import com.itheima.huanxinim.adapter.EMMessageListenerAdapter
 import com.itheima.huanxinim.base.BaseActivity
 import com.itheima.huanxinim.factory.FragmentFactory
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,5 +21,32 @@ class MainActivity : BaseActivity() {
             FragmentFactory.instance.getFragment(tabId)?.let { beginTransaction.replace(R.id.fragment_frame, it) }
             beginTransaction.commit()
         }
+
+        EMClient.getInstance().chatManager().addMessageListener(messageListener)
+    }
+
+    //接收消息回调
+    private var messageListener = object : EMMessageListenerAdapter() {
+        override fun onMessageReceived(p0: MutableList<EMMessage>?) {
+            Log.e("messageListener==", "收到消息")
+            updateBottomBarUnRead()
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        updateBottomBarUnRead()
+    }
+
+    private fun updateBottomBarUnRead() {
+        runOnUiThread {
+            //初始化bottombar未读消息的个数
+            val tab = bottomBar.getTabWithId(R.id.tab_conversation)
+            tab.setBadgeCount(EMClient.getInstance().chatManager().unreadMessageCount)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        EMClient.getInstance().chatManager().removeMessageListener(messageListener)
     }
 }
